05c9105e6d2d6b6c7e7c8c77ce6326ac6a00bb20
David <David@Davidjustiniano>
2024-09-03 23:53:43 -0600

    hola

--- src/main/java/com/mycompany/ventas/Ventas.java
+++ src/main/java/com/mycompany/ventas/Ventas.java
@@ -11,6 +11,6 @@ package com.mycompany.ventas;
 public class Ventas {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worldashbdbhsdbfhdsfbhdshfbsdbfbsbhnmmbnmnmnm  bhhjghjfhjdhjbfb!");
+        System.out.println("Hello Worldashbdbhsdbfhdsfbhdshfbsdbfbsbhgfhcgfhjgjghjghjghjghjghjyjghjnmmbnmnmnm  bhhjghjfhjdhjbfb!");
     }
 }
