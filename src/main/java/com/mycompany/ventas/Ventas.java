6a9d7a3d8d97d7f3f01af7587ed0066dedd28bd6
David <David@Davidjustiniano>
2024-09-10 20:38:49 -0600

    PRUEBAS

--- src/main/java/com/mycompany/ventas/Ventas.java
+++ src/main/java/com/mycompany/ventas/Ventas.java
@@ -11,6 +11,6 @@ package com.mycompany.ventas;
 public class Ventas {
 
     public static void main(String[] args) {
-        System.out.println("PRUEBA ");
+        System.out.println("PRUEBA DE  ");
     }
 }
